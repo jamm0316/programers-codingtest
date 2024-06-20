0dff749b62b2d94e8aa89507058ace74d9cda3b0
jamm0316 <168372878+jamm0316@users.noreply.github.com>
2024-06-21 06:45:41 +0900

    Update 글자만들기.js

--- lv.0/글자만들기/글자만들기.js
+++ lv.0/글자만들기/글자만들기.js
@@ -1,1 +1,14 @@
-cre
+function solution(my_string, indices) {
+    let char_array = my_string.split('');
+    
+    indices.sort((a, b) => b - a);
+    
+    for (let index of indices) {
+        char_array.splice(index, 1);
+    }
+    return char_array.join('');
+}
+
+const my_string = "apporoograpemmemprs"
+const indices = [1, 16, 6, 15, 0, 10, 11, 3]
+console.log(solution(my_string, indices))
