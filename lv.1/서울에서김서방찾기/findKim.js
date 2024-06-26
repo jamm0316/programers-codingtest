74851f7307693d8d6e525cbdb748f2498878f746
jamm0316 <168372878+jamm0316@users.noreply.github.com>
2024-06-26 21:46:51 +0900

    Update findKim.js

--- lv.1/서울에서김서방찾기/findKim.js
+++ lv.1/서울에서김서방찾기/findKim.js
@@ -1,7 +1,19 @@
+// case1.
 function solution(seoul){
     for (i = 0; i < seoul.length; i++) {
         if (seoul[i] === "Kim") {
             return "김서방은 " + i +"에 있다"
+        }
+    }
+}
+
+
+//case2. 포맷팅 이용
+function solution(seoul){
+    for (i = 0; i < seoul.length; i++) {
+        if (seoul[i] === "Kim") {
+            const index = i;
+            return `김서방은 ${index}에 있다`;
         }
     }
 }
