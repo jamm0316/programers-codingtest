5786d25b52dee812651693213a8110e839d2ea6c
jamm0316 <168372878+jamm0316@users.noreply.github.com>
2024-06-30 16:32:41 +0900

    Create while or stack.js

---
+++ lv.2/짝지어 제거하기/while or stack.js
@@ -0,0 +1,42 @@
+// case 1. while -> O(n^2)
+function solution(s) {
+    // init 's' to array
+    s = s.split('');
+    
+    // roof for splice
+    let i = 0;
+    while (i < s.length) {
+        if (s[i] === s[i + 1]) {
+            s.splice(i, 2);
+            i = 0;
+        } else {
+            i++;
+        }
+    }
+    
+    // test array is empthy
+    if (s.length === 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+// case 2. while -> O(n)
+function solution(s) {
+    // init stack
+    let stack = [];
+    
+    // iterate through string characters
+    for (let char of s) {
+        // if stack is not empty and the top element is the same as the current character
+        if (stack.length > 0 && stack[stack.length - 1] === char) {
+            stack.pop();  // remove the top element
+        } else {
+            stack.push(char);  // add the current character to the stack
+        }
+    }
+    
+    // if stack is empty, return 1, else return 0
+    return stack.length === 0 ? 1 : 0;
