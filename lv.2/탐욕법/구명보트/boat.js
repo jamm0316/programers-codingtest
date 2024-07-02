89d053ed090300f10b24cd6d38f17c07c5c7c193
jamm0316 <168372878+jamm0316@users.noreply.github.com>
2024-07-03 06:49:06 +0900

    Update boat.js

--- lv.2/탐욕법/구명보트/boat.js
+++ lv.2/탐욕법/구명보트/boat.js
@@ -1,9 +1,24 @@
-// case 1. 정렬 후 구하기
+// case 1. 정렬 후 양끝에서 접근
+function solution(people, limit) {
+    let count = 0
+    people.sort((a, b) => a - b);
+    
+    let light = 0;  // index of most light
+    let heavy = people.length - 1;  // index of most heavy
+    let boat = 0;  // number of boat
+    
+    while (light <= heavy) {  
+        if (people[light] + people[heavy] <= limit) {  // check on a boat together
+            light++  // carring light person
+        }
+        heavy--  // carring heavy person
+        boat++  // go boat
+    }
+    return boat
+}
 
 
-
-// 실패하는 티케이스가 무엇인지 모르겠음...;;
-
+// case 2. stack으로 비교하며 접근
 function solution(people, limit) {
     let boat = [];
     let count = 0;
