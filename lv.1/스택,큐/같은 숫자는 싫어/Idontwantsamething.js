7f31370513a0809c09677c25e2e42481e3ffde55
jamm0316 <168372878+jamm0316@users.noreply.github.com>
2024-07-11 14:57:23 +0900

    Create Idontwantsamething.js

---
+++ lv.1/스택,큐/같은 숫자는 싫어/Idontwantsamething.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    let stack = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== stack[stack.length - 1]) {
+            stack.push(arr[i]);
+        }
+    }
+    return stack
+}
